48cf3004f8954eb245dec7621798a17b728f9c46
icebear <140247789+icebear0111@users.noreply.github.com>
2023-07-25 20:17:17 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 72 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point {
+    int x, y;
+} point;
+
+int compare(const void *a, const void *b);
+
+int main() {
+    int N;
+    point *A;
+
+    scanf("%d", &N);
+    A = (point*)malloc(sizeof(point) * N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &A[i].x, &A[i].y);
+    }
+    
+    qsort(A, N, sizeof(point), compare);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", A[i].x, A[i].y);
+    }
+    
+    free(A);
+    
+    return 0;
+}
+
+int compare(const void *a, const void *b) {
+    point A = *(point *)a;
+    point B = *(point *)b;
+
+    if (A.x > B.x) {
+        return 1;
+    }
+    else if (A.x == B.x) {
+        if (A.y > B.y) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+    return -1;
+}
