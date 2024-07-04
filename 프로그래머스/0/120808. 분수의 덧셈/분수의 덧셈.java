46069eec05396eefec40159523f90229b230f48b
icebear <140247789+icebear0111@users.noreply.github.com>
2024-07-04 13:18:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int x = (numer1 * denom2) + (numer2 * denom1);
+        int y = denom1 * denom2;
+        int gcd, x1, y1;
+        
+        gcd = 0;
+        x1 = x;
+        y1 = y;
+        while (y1 != 0) {
+            int temp = y1;
+            y1 = x1 % y1;
+            x1 = temp;
+        }
+        gcd = x1;
+        
+        x = x / gcd;
+        y = y / gcd;
+        
+        
+        int[] answer = {x, y};
+        return answer;
+    }
+}
