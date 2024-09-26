9a9f8c558801e225b174b4ef30a25d768ec69e56
Lim Jiseob <wltjq0901@naver.com>
2024-09-27 02:42:18 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[8];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        String answer = "";
+        for (int i = 0; i < 7; i++) {
+            if (arr[i + 1] == arr[i] + 1) {
+                answer = "ascending";
+            }
+            else if (arr[i + 1] == arr[i] - 1) {
+                answer = "descending";
+            }
+            else {
+                answer = "mixed";
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
