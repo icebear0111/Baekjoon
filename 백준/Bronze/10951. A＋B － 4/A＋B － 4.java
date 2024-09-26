e59e8fc446e84068bc794514310e4b9da9bb1313
Lim Jiseob <wltjq0901@naver.com>
2024-09-26 23:59:38 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(A + B);
+        }
+    }
+}
