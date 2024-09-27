d439d382cb7c15a98297b566b4b866e2a39d1ca7
Lim Jiseob <wltjq0901@naver.com>
2024-09-27 19:55:10 +0900

    [Bronze II] Title: 분해합, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < N; i++) {
+            int num = i;
+            int sum = i;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum == N) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+}
