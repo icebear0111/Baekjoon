b5859627ce36e88823aedad7da302a93f830b3fd
Lim Jiseob <wltjq0901@naver.com>
2024-10-01 04:25:00 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 324 ms, Memory: 23236 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<String> stack = new Stack<>();
+        String input = "";
+        while (true) {
+            input = br.readLine();
+            stack.clear();
+            if (Objects.equals(input, ".")) {
+                break;
+            }
+            boolean flag = true;
+            for (int i = 0; i < input.length(); i++) {
+                if (Objects.equals(input.charAt(i), '(') || Objects.equals(input.charAt(i), '[')) {
+                    stack.push(String.valueOf(input.charAt(i)));
+                }
+                if (Objects.equals(input.charAt(i), ')')) {
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    if (Objects.equals(stack.pop(), "(")) {
+                        continue;
+                    }
+                    else {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (Objects.equals(input.charAt(i), ']')) {
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    if (Objects.equals(stack.pop(), "[")) {
+                        continue;
+                    }
+                    else {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                if (flag) {
+                    System.out.println("yes");
+                }
+                else {
+                    System.out.println("no");
+                }
+            }
+            else {
+                System.out.println("no");
+            }
+        }
+    }
+}
