cb1cd10f177fb4381280f1190e99fcbd1f8177af
Lim Jiseob <wltjq0901@naver.com>
2024-10-06 03:30:45 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 548 ms, Memory: 60576 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String site = st.nextToken();
+            String password = st.nextToken();
+            map.put(site, password);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String site = br.readLine();
+            bw.write(map.get(site) + "\n");
+        }
+
+        bw.flush();
+    }
+}
