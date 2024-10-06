74e0a15bd98cc5f9ae9adf40399db031d0c8fb89
Lim Jiseob <wltjq0901@naver.com>
2024-10-07 00:00:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 15760 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -8,13 +8,13 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        int[] dp = new int[12];
-        dp[0] = 0;
+        int[] dp = new int[11];
+        
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-        for (int i = 4; i < 12; i++) {
+        for (int i = 4; i < 11; i++) {
             dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
 
