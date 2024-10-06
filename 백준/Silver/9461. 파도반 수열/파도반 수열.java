3360c53697c7842197ed502e63fa9c841f6a0b8a
Lim Jiseob <wltjq0901@naver.com>
2024-10-07 00:12:01 +0900

    [Silver III] Title: 파도반 수열, Time: 116 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i < 101; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            bw.write(dp[N] + "\n");
+        }
+        bw.flush();
+    }
+}
