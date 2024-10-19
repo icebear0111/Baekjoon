9857b5864e4e475cd1ef9f4536416c3ae193d917
Lim Jiseob <wltjq0901@naver.com>
2024-10-19 17:26:34 +0900

    [Bronze V] Title: Java Warriors, Time: 96 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int answer = n * 4000;
+        bw.write(answer + "");
+        bw.flush();
+    }
+}
