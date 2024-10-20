e46974b4d585b5e29df721ad4176227636d30eaf
Lim Jiseob <wltjq0901@naver.com>
2024-10-20 19:05:37 +0900

    [Bronze IV] Title: 헛간 청약, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(Math.min(N, (W / L) * (H / L))));
+        bw.flush();
+        bw.close();
+    }
+}
