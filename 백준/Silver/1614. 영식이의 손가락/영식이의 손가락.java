e4da6d182567ab307e33821228ce4bd036ddb9b6
Lim Jiseob <wltjq0901@naver.com>
2024-12-09 16:16:18 +0900

    [Silver III] Title: 영식이의 손가락, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.java
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.java
@@ -11,20 +11,12 @@ public class Main {
 
         if (start == 1 || start == 5)
             result = n * 8 + (start == 1 ? 0 : 4);
-        else if (start == 2) {
-            if (n % 2 == 0)
-                result = n * 4 + 1;
-            else
-                result = n * 4 + 3;
-        }
+        else if (start == 2)
+            result = n * 4 + (n % 2 == 0 ? 1 : 3);
         else if (start == 3)
             result = n * 4 + 2;
-        else {
-            if (n % 2 == 0)
-                result = n * 4 + 3;
-            else
-                result = n * 4 + 1;
-        }
+        else
+            result = n * 4 + (n % 2 == 0 ? 3 : 1);
 
         System.out.println(result);
     }
